7a4ac6bef99417a7fd6882f56e6bfa55070e4cc5
Bach-An <nguyen.b@northeastern.edu>
2021-01-12 16:00:29 -0500

    Added height ui element

---
+++ Assets/Height.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Height : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Text heightText;
+
+
+
+    private void Update()
+    {
+        float curScore = float.Parse(heightText.text);
+        float playerPos = player.position.y - 1;
+
+        heightText.text = playerPos.ToString("0");
+
+    }
+
+}
+
